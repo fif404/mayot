8b9776b6c80f513d4b9b1bf7ba150398cf710fc1
fif <fif@DESKTOP-BFHUIKE>
2022-11-03 10:28:43 +1200

    Добавьте файлы проекта.

---
+++ mayot/Program.cs
@@ -0,0 +1,17 @@
+﻿// Скрипников Сергей 17 вариант 2.3
+Console.WriteLine("Ввеите x1:");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ввеите y1:");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ввеите x2:");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ввеите y2:");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ввеите x3:");
+double x3 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ввеите y3:");
+double y3 = double.Parse(Console.ReadLine());
+if ((x1 == 9) && (y1 == 2) || (x2 == 7) && (y2 == 3) || (x3 == 0.1) && (y3 == 4))
+    Console.WriteLine("точки лежат в первой части");
+else
+    Console.WriteLine("точки не лежат в первой части");
